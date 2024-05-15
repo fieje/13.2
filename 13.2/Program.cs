c6d549af159ab2ce5275c4719625f2a9ef0eab30
jack <144657795+fieje@users.noreply.github.com>
2024-05-15 21:37:50 +0300

    Add files via upload

---
+++ 13.2/Program.cs
@@ -0,0 +1,53 @@
+﻿//Програма для обчислення математичного виразу
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float x, y, z;
+        float numerator, denominator, result;
+
+        Console.WriteLine("Enter the value of x: ");
+        x = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the value of y: ");
+        y = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the value of z: ");
+        z = float.Parse(Console.ReadLine());
+
+        if (z > 10)
+        {
+            numerator = Math.Max((float)Math.Pow(x, 3), (float)Math.Pow((x + y), 2));
+        }
+        else
+        {
+            numerator = 0; 
+        }
+
+        if (z <= 10)
+        {
+            denominator = Math.Min(Math.Abs(x - 2), Math.Abs(x + z));
+        }
+        else
+        {
+            denominator = 1; 
+        }
+
+        if (denominator == 0)
+        {
+            Console.WriteLine("Error: Division by zero.");
+            return;
+        }
+
+        result = numerator / denominator;
+
+        Console.WriteLine("Result: " + result);
+        Console.ReadLine();
+    }
+}
+
+//x=2
+//y=3
+//z=12
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class MathExpressionTest
+    {
+        [TestMethod]
+        public void TestMathExpression()
+        {
+            float x = 2;
+            float y = 3;
+            float z = 12;
+            float expectedResult = Math.Max((float)Math.Pow(x, 3), (float)Math.Pow((x + y), 2));
+
+            float result = CalculateMathExpression(x, y, z);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+
+        private float CalculateMathExpression(float x, float y, float z)
+        {
+            float numerator, denominator;
+
+            if (z > 10)
+            {
+                numerator = Math.Max((float)Math.Pow(x, 3), (float)Math.Pow((x + y), 2));
+            }
+            else
+            {
+                numerator = 0;
+            }
+
+            if (z <= 10)
+            {
+                denominator = Math.Min(Math.Abs(x - 2), Math.Abs(x + z));
+            }
+            else
+            {
+                denominator = 1;
+            }
+
+            if (denominator == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return numerator / denominator;
+        }
+    }
+}
